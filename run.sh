55ae1a3e19dc293cf34537b72791089bfb6479d4
Rain <57244491+rainhu22@users.noreply.github.com>
2024-01-02 12:32:51 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,5 @@ python main.py --model Conv_former \
                 --data-set RSNA \
                 --img-list rsna \
                 --data-path /data-8T/hyr/MCTformer/Datasets/rsna \
-                --attention-type cls \
-                --layer-index 4 \
                 --output_dir /data-8T/hyr/MCTformer/result_rsna/ \
                 --finetune /data-8T/
